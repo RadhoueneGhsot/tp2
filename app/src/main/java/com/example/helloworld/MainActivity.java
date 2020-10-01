4cd302c7dbe3f4f05229168f79ece2a0a4dd46a2
POSTE6 <radhoueneh@gmail.com>
2020-10-01 16:49:14 +0100

    hello

--- app/src/main/java/com/example/helloworld/MainActivity.java
+++ app/src/main/java/com/example/helloworld/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("MainActivity", "Hello World");
-
+        Log.d("hello hhhhhh", "Hello World");
     }
 
 }
